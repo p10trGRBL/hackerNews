ae1d7c02d8fc5ac615c9b52a35a0f9e95bb2677a
unknown <shavkat95@icloud.com>
2023-08-30 17:26:51 +0200

    some styles

--- src/Article.jsx
+++ src/Article.jsx
@@ -1,29 +1,38 @@
-import { useEffect } from 'react';
-import './Article.css';
+import { useEffect } from "react";
+import "./Article.css";
 
-
-
-export default function Article ({article, index}) {
-
-    let url = article.url;
-    let domain = "";
-    if (url) {
-        domain = url.split('/');
-        domain = domain[2];
-        if(domain.startsWith('www.')){
-            domain = domain.substring(4);
-        }
+export default function Article({ article, index }) {
+  let url = article.url;
+  let domain = "";
+  if (url) {
+    domain = url.split("/");
+    domain = domain[2];
+    if (domain.startsWith("www.")) {
+      domain = domain.substring(4);
     }
+  }
+
+  useEffect(() => {}, []);
+
+  return (
     
+    <div className="article">
+      <span className="first_line">
+        <span className="index">{index + 1}</span>
+        <a className="title" href={url || ""}>
+          {article.title}
+        </a>
+        <a className="domain" href={domain || ""}>
+          ({domain || ""})
+        </a>
+      </span>
 
+      <span className="second_line">
+        <span className="points">{article.points} points | </span>
+        <span className="comments"> {article.num_comments} comments </span>
+      </span>
 
-    useEffect(()=>{
-    },[])
-
-    return (<div className='article'>
-        <span>{index+1}</span>
-        <a className='title' href={url || ""}>{article.title}</a>
-        <a className='domain' href={domain || ""}>({domain || ""})</a>
-    </div>);
+      <br />
+    </div>
+  );
 }
-
